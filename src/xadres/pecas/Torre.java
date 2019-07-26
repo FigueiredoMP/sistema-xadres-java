a34e180d2db192aab59dc11983a3249b2e1c85c0
FigueiredoMP <joaopause@hotmail.com>
2019-07-26 14:00:48 -0300

    Implementado movimento de torre, porém ainda com falha

--- src/xadres/pecas/Torre.java
+++ src/xadres/pecas/Torre.java
@@ -5,7 +5,7 @@ import boardgame.Tabuleiro;
 import xadres.Cor;
 import xadres.pecaXadres;
 
-public class Torre extends pecaXadres{
+public class Torre extends pecaXadres {
 
 	public Torre(Tabuleiro tabuleiro, Cor cor) {
 		super(tabuleiro, cor);
@@ -15,24 +15,55 @@ public class Torre extends pecaXadres{
 	public String toString() {
 		return "T";
 	}
-	
+
 	@Override
 	public boolean[][] movimentoPossivel() {
 		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
-		
-		Posicao p = new Posicao(0,0);
-		
+
+		Posicao p = new Posicao(0, 0);
+
 		// acima
 		p.setValores(posicao.getLinha() - 1, posicao.getColuna());
-		while(getTabuleiro().posicaoExistente(p) && !getTabuleiro().existePeca(p)) {
+		while (getTabuleiro().posicaoExistente(p) && !getTabuleiro().existePeca(p)) {
 			mat[p.getLinha()][p.getColuna()] = true;
-			p.setlinha(p.getLinha() -1);
+			p.setlinha(p.getLinha() - 1);
 		}
-		if (getTabuleiro().posicaoExistente(p) && ePecaOponente(p)){
+		if (getTabuleiro().posicaoExistente(p) && ePecaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
+		// esquerda
+		p.setValores(posicao.getLinha(), posicao.getColuna() -1);
+		while (getTabuleiro().posicaoExistente(p) && !getTabuleiro().existePeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setcoluna(p.getColuna() - 1);
+		}
+		if (getTabuleiro().posicaoExistente(p) && ePecaOponente(p)) {
 			mat[p.getLinha()][p.getColuna()] = true;
 		}
 		
+		//direita
+		p.setValores(posicao.getLinha(), posicao.getColuna() +1);
+		while (getTabuleiro().posicaoExistente(p) && !getTabuleiro().existePeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setcoluna(p.getColuna() + 1);
+		}
+		if (getTabuleiro().posicaoExistente(p) && ePecaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		//baixo
+		p.setValores(posicao.getLinha() + 1, posicao.getColuna());
+		while (getTabuleiro().posicaoExistente(p) && !getTabuleiro().existePeca(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setlinha(p.getLinha() + 1);
+		}
+		if (getTabuleiro().posicaoExistente(p) && ePecaOponente(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+
 		return mat;
 	}
-	
+
 }
