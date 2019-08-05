4cfddea8bc5eebae944d66c6216630677bc28072
FigueiredoMP <joaopause@hotmail.com>
2019-08-05 12:27:06 -0300

    Peao

--- src/xadres/partidaXadres.java
+++ src/xadres/partidaXadres.java
@@ -2,12 +2,12 @@ package xadres;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import boardgame.Peca;
 import boardgame.Posicao;
 import boardgame.Tabuleiro;
+import xadres.pecas.Peao;
 import xadres.pecas.Rei;
 import xadres.pecas.Torre;
 
@@ -194,19 +194,29 @@ public class partidaXadres {
 	}
 
 	private void configuracaoInicial() {
-		posicaoNovaPeca('c', 1, new Torre(tabuleiro, Cor.BRANCO));
-		posicaoNovaPeca('c', 2, new Torre(tabuleiro, Cor.BRANCO));
-		posicaoNovaPeca('d', 2, new Torre(tabuleiro, Cor.BRANCO));
-		posicaoNovaPeca('e', 2, new Torre(tabuleiro, Cor.BRANCO));
-		posicaoNovaPeca('e', 1, new Torre(tabuleiro, Cor.BRANCO));
-		posicaoNovaPeca('d', 1, new Rei(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('a', 1, new Torre(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('h', 1, new Torre(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('a', 2, new Peao(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('b', 2, new Peao(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('c', 2, new Peao(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('d', 2, new Peao(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('e', 2, new Peao(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('f', 2, new Peao(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('g', 2, new Peao(tabuleiro, Cor.BRANCO));
+		posicaoNovaPeca('h', 2, new Peao(tabuleiro, Cor.BRANCO));
 
-		posicaoNovaPeca('c', 7, new Torre(tabuleiro, Cor.PRETO));
-		posicaoNovaPeca('c', 8, new Torre(tabuleiro, Cor.PRETO));
-		posicaoNovaPeca('d', 7, new Torre(tabuleiro, Cor.PRETO));
-		posicaoNovaPeca('e', 7, new Torre(tabuleiro, Cor.PRETO));
-		posicaoNovaPeca('e', 8, new Torre(tabuleiro, Cor.PRETO));
-		posicaoNovaPeca('d', 8, new Rei(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('a', 8, new Torre(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('h', 8, new Torre(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('a', 7, new Peao(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('b', 7, new Peao(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('c', 7, new Peao(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('d', 7, new Peao(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('e', 7, new Peao(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('f', 7, new Peao(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('g', 7, new Peao(tabuleiro, Cor.PRETO));
+		posicaoNovaPeca('h', 7, new Peao(tabuleiro, Cor.PRETO));
 
 	}
 
